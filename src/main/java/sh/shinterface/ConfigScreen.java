0dd062d5c0d8afac473774079365ae871349b58c
CCookii <yanis.peters.depinte@gmail.com>
2022-04-03 21:00:18 +0200

    Added Role Selection

--- src/main/java/sh/shinterface/ConfigScreen.java
+++ src/main/java/sh/shinterface/ConfigScreen.java
@@ -1,5 +1,7 @@
 package sh.shinterface;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
@@ -7,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -45,33 +49,60 @@ public class ConfigScreen extends StackPane {
     private final ToggleGroup activePlayerGroup = new ToggleGroup();
 
     /**
+     * Box that holds the role selection
+     */
+    private final ChoiceBox<String> roleBox = new ChoiceBox<>(
+            FXCollections.observableArrayList("Liberal", "Fascist", "Hitler")
+    );
+
+    /**
      * Creates a new ConfigurationScreen that is shown on the given stage
      * @param stage Stage that displays the ConfigScreen
      */
     public ConfigScreen(Stage stage) {
         this.stage = stage;
-        ChoiceBox<Integer> choiceBox = new ChoiceBox<>(); // Selectie voor aantal spelers
-        // Add the options
-        for (int i = minPlayers; i <= maxPlayers; i++) {
-            choiceBox.getItems().add(i);
-        }
-        choiceBox.setValue(minPlayers); // Standaard-waarde is het minimum
-        choiceBox.setOnAction(this::updatePlayers); // Na selectie wordt het aantal spelers ge-update
 
-        Button createGameButton = new Button("Create Game");
-        createGameButton.setOnAction(Main::confirmSelection);
+        // INIT Controls
 
-        VBox controlsBox = new VBox(); // Container van de controls
-        VBox playerContainer = new VBox();
-        playerFields = playerContainer.getChildren();
-        controlsBox.getChildren().addAll(choiceBox, playerContainer, createGameButton);
+         // INIT ChoiceBox to select the amount of players
+          ChoiceBox<Integer> choiceBox = new ChoiceBox<>(); // Selectie voor aantal spelers
+          // Add the options
+          for (int i = minPlayers; i <= maxPlayers; i++) {
+          choiceBox.getItems().add(i);
+          }
+          choiceBox.setValue(minPlayers); // Standaard-waarde is het minimum
+          choiceBox.setOnAction(this::updatePlayers); // Na selectie wordt het aantal spelers ge-update
+         // END CHOICE BOX
+
+         // INIT Player fields
+          VBox playerContainer = new VBox();
+          playerFields = playerContainer.getChildren(); // Sla de fields op voor makkelijk aan te passen
+          choiceBox.fireEvent(new ActionEvent()); // Roept meteen updatePlayers op
+          activePlayerGroup.selectedToggleProperty().addListener(this::updateRoleChoice);
+          // Zorgt dat de role choice wordt getoond na selecteren van actieve speler
+         //END PLAYER FIELDS
+
+         // INIT Role Selection
+          HBox hBox = new HBox(new Label("Role:"), roleBox);
+          hBox.setVisible(false); // Eerst niet zichtbaar
+          HBox.setHgrow(hBox, Priority.ALWAYS); // Fills window width
+          hBox.getStyleClass().add("role-box");
+         // END ROLE SELECTION
+
+         // INIT Create Game Button
+          Button createGameButton = new Button("Create Game");
+          createGameButton.setOnAction(Main::confirmSelection);
+         // END BUTTON
+
+         VBox controlsBox = new VBox(choiceBox, playerContainer, new HBox(hBox, createGameButton));
+         controlsBox.getStyleClass().add("inner-box");
+        // END CONTROLS
+
         Label title = new Label("SECRET HITLER"); // Titel van het configuratiescherm
         title.getStyleClass().add("title");
-        controlsBox.getStyleClass().add("inner-box");
-        this.getStyleClass().add("config-screen");
-        this.getChildren().addAll(controlsBox, title);
 
-        choiceBox.fireEvent(new ActionEvent()); // Roept meteen updatePlayers op
+        this.getStyleClass().addAll("config-screen", "liberal");
+        this.getChildren().addAll(controlsBox, title);
     }
 
     /**
@@ -92,9 +123,11 @@ public class ConfigScreen extends StackPane {
                 playerFields.add(playerField);
                 activePlayerGroup.getToggles().add(playerField.getButton());
             }
+        } else {
+            activePlayerGroup.getToggles().remove(amount, playerFields.size());
+            playerFields.remove(amount, playerFields.size()); // Hou enkel het juiste aantal spelers over
+            updateRoleChoice(null);
         }
-        playerFields.remove(amount, playerFields.size()); // Hou enkel het juiste aantal spelers over
-        activePlayerGroup.getToggles().remove(amount, playerFields.size());
         stage.sizeToScene(); // Resize het scherm zodat de player fields zichtbaar zijn
     }
 
@@ -116,5 +149,10 @@ public class ConfigScreen extends StackPane {
             }
         }
         return (valid) ? players : List.of();
+    }
+
+    public void updateRoleChoice(Observable observable) {
+        roleBox.getParent().setVisible(activePlayerGroup.getSelectedToggle() != null);
+        roleBox.setValue("");
     }
 }
