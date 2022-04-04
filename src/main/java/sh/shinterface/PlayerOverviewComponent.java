edbba16140b72e3c1e69c156a992d052c6bd29b6
Vincent Batens <vincent.batens@ugent.be>
2022-04-05 01:06:25 +0200

    Added playerOverview + errorText of 1025 lines :/

--- src/main/java/sh/shinterface/PlayerOverviewComponent.java
+++ src/main/java/sh/shinterface/PlayerOverviewComponent.java
@@ -6,15 +6,50 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerOverviewComponent extends VBox {
 
-    public PlayerOverviewComponent(Player player, Game game) {
+    private static final Set<Role> FASCROLES = new HashSet<>() {{
+        add(Role.HITLER);
+        add(Role.FASCIST);
+    }};
+
+    public PlayerOverviewComponent(Player player, Game game, RightUpperWindow parent) {
         ImageView susImage = new ImageView(ImagePicker.pick(player.getSuspectedFaction()));
         Label playerLabel = new Label(new PlayerStringConverter(game).toString(player));
         ChoiceBox<Role> roleChoiceBox = new ChoiceBox<>(
                 FXCollections.observableArrayList(Role.UNKNOWN, Role.LIBERAL, Role.FASCIST, Role.HITLER)
         );
         roleChoiceBox.setValue(Role.UNKNOWN);
+
+        roleChoiceBox.valueProperty().addListener((observableValue, oldRole, newRole) -> {
+            int hitler = 0;
+            int fasc = 0;
+
+            if (oldRole.equals(Role.HITLER) ^ newRole.equals(Role.HITLER)) {
+                if (oldRole.equals(Role.HITLER)) {
+                    hitler = -1;
+                } else {
+                    hitler = 1;
+                }
+            }
+
+            if (FASCROLES.contains(oldRole) ^ FASCROLES.contains(newRole)) {
+                if (FASCROLES.contains(oldRole)) {
+                    fasc = -1;
+                } else {
+                    fasc = 1;
+                }
+            }
+
+            if (!parent.updateFasc(fasc, hitler, susImage, ImagePicker.pick(newRole))) {
+                roleChoiceBox.setValue(oldRole);
+            }
+
+        });
+
         try {
             if (game.getActivePlayer().equals(player)) {
                 roleChoiceBox.setDisable(true);
