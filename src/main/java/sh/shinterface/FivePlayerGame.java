f4e76e79603a2023499ef4c6077b34b0264aeab9
Vincent Batens <vincent.batens@ugent.be>
2022-04-04 18:06:53 +0200

    Restructured FivePlayerGame

--- src/main/java/sh/shinterface/FivePlayerGame.java
+++ src/main/java/sh/shinterface/FivePlayerGame.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class FivePlayerGame extends Bord1Game {
 
     private final List<Player> players;
-    private TableView<Gov> govTable;
+    private FivePlayerGameWindow gameVenster;
 
     public FivePlayerGame(List<Player> players) {
         this.players = players;
@@ -22,26 +22,8 @@ public class FivePlayerGame extends Bord1Game {
     @Override
     public void start() {
         Stage stage = new Stage();
-        SplitPane gameVenster = new SplitPane();
+        gameVenster = new FivePlayerGameWindow(this);
         Scene scene = new Scene(gameVenster);
-
-        VBox leftSide = new VBox();
-        govTable = new TableView<>();
-
-        TableColumn<Gov, String> president = new TableColumn<>("President");
-        TableColumn<Gov, String> chancellor = new TableColumn<>("Chancellor");
-        TableColumn<Gov, String> claim = new TableColumn<>("Claim(s)");
-
-        president.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().president().getId() + ". " + data.getValue().president().getName()));
-        chancellor.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().chancellor().getId() + ". " + data.getValue().chancellor().getName()));
-        claim.setCellValueFactory(data -> data.getValue().displayClaims());
-
-        govTable.getColumns().setAll(president, chancellor, claim);
-
-        leftSide.getChildren().add(govTable);
-        leftSide.getChildren().add(new NewGovPane(this));
-        gameVenster.getItems().add(leftSide);
-
         stage.setScene(scene);
         stage.show();
     }
@@ -52,6 +34,6 @@ public class FivePlayerGame extends Bord1Game {
     }
 
     public TableView<Gov> getGovTable() {
-        return govTable;
+        return gameVenster.getGovTable();
     }
 }
