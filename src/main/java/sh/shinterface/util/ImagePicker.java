966ffe6f8db12afdbd8cec74f1acfdd692ef98b0
CCookii <yanis.peters.depinte@gmail.com>
2022-04-05 20:48:48 +0200

    made dir for images

--- src/main/java/sh/shinterface/util/ImagePicker.java
+++ src/main/java/sh/shinterface/util/ImagePicker.java
@@ -8,22 +8,22 @@ import java.util.Random;
 
 public class ImagePicker {
 
-    private static final Image UNKNOWN = new Image("sh/shinterface/secretrole.png");
-    private static final Image HITLER = new Image("sh/shinterface/hitler0.png");
+    private static final Image UNKNOWN = new Image("sh/shinterface/images/secretrole.png");
+    private static final Image HITLER = new Image("sh/shinterface/images/hitler0.png");
 
     private static final List<Image> FASC = List.of(
-            new Image("sh/shinterface/fascist0.png"),
-            new Image("sh/shinterface/fascist1.png"),
-            new Image("sh/shinterface/fascist2.png")
+            new Image("sh/shinterface/images/fascist0.png"),
+            new Image("sh/shinterface/images/fascist1.png"),
+            new Image("sh/shinterface/images/fascist2.png")
     );
 
     private static final List<Image> LIB = List.of(
-            new Image("sh/shinterface/liberal0.png"),
-            new Image("sh/shinterface/liberal1.png"),
-            new Image("sh/shinterface/liberal2.png"),
-            new Image("sh/shinterface/liberal3.png"),
-            new Image("sh/shinterface/liberal4.png"),
-            new Image("sh/shinterface/liberal5.png")
+            new Image("sh/shinterface/images/liberal0.png"),
+            new Image("sh/shinterface/images/liberal1.png"),
+            new Image("sh/shinterface/images/liberal2.png"),
+            new Image("sh/shinterface/images/liberal3.png"),
+            new Image("sh/shinterface/images/liberal4.png"),
+            new Image("sh/shinterface/images/liberal5.png")
     );
 
     public static Image pick(Role role) {
